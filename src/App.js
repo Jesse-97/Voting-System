d1ff39d0168b778045b6af5bc8bcae7033142ce5
Jesse-97 <bgreeman97@gmail.com>
2025-08-11 03:25:55 +0530

    updated lodin & register page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Login from "./login.js";
+import Register from "./register.js";
 
 export function Login_page() {
   return <Login />;
 }
 
+export function Register_page() {
+  return <Register />;
+}
 export default function App() {
-  return <Login_page />;
+  const currentPage = <Login />;
+  return currentPage;
 }
--- src/register.js
+++ src/register.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./login.css";
 
-export default function Login() {
+export default function Register() {
   return (
     <>
       <div className="background hide"></div>
@@ -33,28 +33,28 @@ export default function Login() {
             </svg>
             <input type="password" placeholder="Password..." />
           </div>
+          <div className="input-row-container">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 640 640"
+              style={{ width: "25px", height: "25px" }}
+            >
+              <path
+                fill="#bababa"
+                d="M256 160L256 224L384 224L384 160C384 124.7 355.3 96 320 96C284.7 96 256 124.7 256 160zM192 224L192 160C192 89.3 249.3 32 320 32C390.7 32 448 89.3 448 160L448 224C483.3 224 512 252.7 512 288L512 512C512 547.3 483.3 576 448 576L192 576C156.7 576 128 547.3 128 512L128 288C128 252.7 156.7 224 192 224z"
+              />
+            </svg>
+            <input type="password" placeholder="Re-enter Password..." />
+          </div>
           <div className="button-container">
             <button>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 640 640"
-                width={"30px"}
-                height={"30px"}
-              >
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
                 <path
                   fill="#bababa"
                   d="M320 576C461.4 576 576 461.4 576 320C576 178.6 461.4 64 320 64C178.6 64 64 178.6 64 320C64 461.4 178.6 576 320 576zM361 417C351.6 426.4 336.4 426.4 327.1 417C317.8 407.6 317.7 392.4 327.1 383.1L366.1 344.1L216 344.1C202.7 344.1 192 333.4 192 320.1C192 306.8 202.7 296.1 216 296.1L366.1 296.1L327.1 257.1C317.7 247.7 317.7 232.5 327.1 223.2C336.5 213.9 351.7 213.8 361 223.2L441 303.2C450.4 312.6 450.4 327.8 441 337.1L361 417.1z"
                 />
               </svg>
             </button>
-          </div>
-        </div>
-        <div className="register-container">
-          <div>
-            <h2>Don't have an account?</h2>
-          </div>
-          <div>
-            <button>Register</button>
           </div>
         </div>
       </div>
