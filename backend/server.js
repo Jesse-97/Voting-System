0697b30fd131a56e2a5d8ef987b4c44ad022560c
Jesse-97 <bgreeman97@gmail.com>
2026-06-03 15:08:56 +0530

    fix prisma require path

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 5000;
 
-const prisma = require("../src/lib/prisma.js");
+const prisma = require('./prisma.js');
 
 app.listen(5000, () => {
   console.log(`Server running on port 5000`);
